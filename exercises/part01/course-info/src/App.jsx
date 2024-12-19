c184d3ab0a3a9526b7e193903f1626917d7daba5
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2024-12-18 19:52:45 -0800

    Submission for Part 1(a) - Exercises 1.1 to 1.2

--- exercises/part01/course-info/src/App.jsx
+++ exercises/part01/course-info/src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = (props) => {
+  console.log(props)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+    <>
+      <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <>
+      <p>Number of exercises {props.total}</p>
+    </>
+  )
+}
+
+
+const App = () => {
+  return (
+    <>
+      <Header course="Half Stack application development" />
+      <Part part="Fundamentals of React" exercises="10" />
+      <Part part="Using props to pass data" exercises="7" />
+      <Part part="State of a component" exercises="14" />
+      <Total total={10 + 7 + 14} />
     </>
   )
 }
