277c49af36628989b4ca53246b0fc453b9430fcd
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2024-12-19 14:05:56 -0800

    Commit: Exercise 1.3

--- exercises/part01/course-info/src/App.jsx
+++ exercises/part01/course-info/src/App.jsx
@@ -11,7 +11,7 @@ const Part = (props) => {
   console.log(props)
   return (
     <>
-      <p>{props.part} {props.exercises}</p>
+      <p>{props.name} {props.exercises}</p>
     </>
   )
 }
@@ -27,15 +27,30 @@ const Total = (props) => {
 
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   return (
-    <>
-      <Header course="Half Stack application development" />
-      <Part part="Fundamentals of React" exercises="10" />
-      <Part part="Using props to pass data" exercises="7" />
-      <Part part="State of a component" exercises="14" />
-      <Total total={10 + 7 + 14} />
-    </>
+    <div>
+      <Header course={course} />
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+    </div>
   )
 }
 
 export default App
+
