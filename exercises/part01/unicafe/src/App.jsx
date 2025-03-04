7163bac8de49561ca082acc98029087e4e7f0b70
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2025-03-04 15:53:20 -0800

    1.9: unicafe step 4

--- exercises/part01/unicafe/src/App.jsx
+++ exercises/part01/unicafe/src/App.jsx
@@ -1,47 +1,61 @@
-import { useState } from 'react'
-import './App.css'
-import Statistics from './Statistics'
+import { useState } from "react";
+import "./App.css";
+import Statistics from "./Statistics";
 
-const Header = ({ title }) => <h1>{title}</h1>
+const Header = ({ title }) => <h1>{title}</h1>;
 
-const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
 
 function App() {
-  const [score, setScore] = useState(0)
-  const [rating, setRating] = useState({ 
-    good: 0, 
-    neutral: 0, 
-    bad: 0 
-  })
+  const [score, setScore] = useState(0);
+  const [rating, setRating] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
 
-  const all = rating.good + rating.bad + rating.neutral
-  const average = all && score / all
-  const positive = all && rating.good / all * 100 + ' %'
+  const all = rating.good + rating.bad + rating.neutral;
+  const average = all && score / all;
+  const positive = all && (rating.good / all) * 100 + " %";
 
-    console.log(score, all, average, positive)
+  console.log(score, all, average, positive);
 
   return (
     <>
-      <Header title='give feedback' />
-        {/* Remember that when changing state that is OBJECT, can't just change 1 when using setState() */}
-        <Button text='good' onClick={() => {
+      <Header title="give feedback" />
+      {/* Remember that when changing state that is OBJECT, can't just change 1 when using setState() */}
+      <Button
+        text="good"
+        onClick={() => {
           setRating({ ...rating, good: rating.good + 1 });
-          setScore(score + 1)
-          }} />
-        <Button text='neutral' onClick={() => {
+          setScore(score + 1);
+        }}
+      />
+      <Button
+        text="neutral"
+        onClick={() => {
           setRating({ ...rating, neutral: rating.neutral + 1 });
-          setScore(score + 0)
-          }} />
-        <Button text='bad' onClick={() => {
+          setScore(score + 0);
+        }}
+      />
+      <Button
+        text="bad"
+        onClick={() => {
           setRating({ ...rating, bad: rating.bad + 1 });
-          setScore(score - 1)
-          }} />
+          setScore(score - 1);
+        }}
+      />
 
-      <Header title='statistics' />
-        <Statistics rating={rating} all={all} average={average} positive={positive} />
-            {/* {console.log(rating.good, rating.neutral, rating.bad, rating, setRating)} */}
+      <Header title="statistics" />
+      <Statistics
+        rating={rating}
+        all={all}
+        average={average}
+        positive={positive}
+      />
+      {/* {console.log(rating.good, rating.neutral, rating.bad, rating, setRating)} */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- exercises/part01/unicafe/src/Statistics.jsx
+++ exercises/part01/unicafe/src/Statistics.jsx
@@ -1,18 +1,25 @@
-const Rating = ({ text, rating }) => <p>{text} {rating}</p>
+const Rating = ({ text, rating }) => (
+  <p>
+    {text} {rating}
+  </p>
+);
 
 const Statistics = ({ rating, all, average, positive }) => {
+  if (all) {
+    return (
+      <>
+        <Rating text="good" rating={rating.good} />
+        <Rating text="neutral" rating={rating.neutral} />
+        <Rating text="bad" rating={rating.bad} />
+        <Rating text="all" rating={all} />
+        <Rating text="average" rating={average} />
+        <Rating text="positive" rating={positive} />
+        {/* {console.log(rating.good, rating.neutral, rating.bad, rating, setRating)} */}
+      </>
+    );
+  } else {
+    return <p>No feedback given</p>;
+  }
+};
 
-  return (
-    <>
-      <Rating text='good' rating={rating.good} />
-      <Rating text='neutral' rating={rating.neutral} />
-      <Rating text='bad' rating={rating.bad} />
-      <Rating text='all' rating={all} />
-      <Rating text='average' rating={average} />
-      <Rating text='positive' rating={positive} />
-          {/* {console.log(rating.good, rating.neutral, rating.bad, rating, setRating)} */}
-    </>
-  )
-}
-
-export default Statistics
+export default Statistics;
