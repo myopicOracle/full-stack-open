9840acd77a833b6a9b61e4c52e6bd21a9a791ab3
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2025-03-03 21:45:18 -0800

    Version 1.0 - works well but needs refactor to be DRY

--- exercises/part01/unicafe/src/App.jsx
+++ exercises/part01/unicafe/src/App.jsx
@@ -1,11 +1,45 @@
 import { useState } from 'react'
 import './App.css'
 
+const Header = ({ title }) => {
+  return (
+    <h1>{title}</h1>
+  )
+}
+
+const Button = ({ text, onClick }) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
 function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <>
-      <h1>Unicafe</h1>
+      <Header title='give feedback' />
+        <Button text='good' onClick={handleClickGood} />
+        <Button text='neutral' onClick={handleClickNeutral} />
+        <Button text='bad' onClick={handleClickBad} />
+      <Header title='statistics' />
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
     </>
   )
 }
