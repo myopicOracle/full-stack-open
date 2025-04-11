310cdeaf52efc97c48534605207d2b2ff682c2a2
myopic_oracle <184602237+myopicOracle@users.noreply.github.com>
2025-04-11 00:44:11 -0700

    Fin: Exercise 2.8

--- exercises/part02/2.6-2.10_phonebook/src/App.jsx
+++ exercises/part02/2.6-2.10_phonebook/src/App.jsx
@@ -1,56 +1,66 @@
-import { useState } from 'react'
-import Numbers from './Numbers'
+import { useState } from "react";
+import Numbers from "./Numbers";
 
 const App = () => {
+  const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    {
+      name: "Arto Hellas",
+      phone: "040-1234567",
+    },
+  ]);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     // persons.includes(() => newName)
     // persons.every((person) => person.name !== newName)
-    newName === 'Arto Hellas'
+    newName === "Arto Hellas"
       ? window.alert(`Arto Hellas is already added to phonebook`)
       : persons.some((person) => person.name === newName)
       ? window.alert(`${newName} is already added to phonebook`)
-      : (setPersons((prevPerson) => ([
+      : setPersons((prevPerson) => [
           ...prevPerson,
           {
-            name: newName
-          }
-        ])))
-      setNewName('')
-  }
+            name: newName,
+            phone: newPhone,
+          },
+        ]);
+    setNewName("");
+    setNewPhone("");
+  };
 
-  const handleInput = (e) => {
-    const nameInput = e.target.value
-    setNewName(nameInput)
-  }
+  const handleName = (e) => {
+    const input = e.target.value;
+    setNewName(input);
+  };
+
+  const handlePhone = (e) => {
+    const input = e.target.value;
+    setNewPhone(input);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: 
-            <input 
-              type='text' 
-              value={newName} 
-              onChange={handleInput} 
-            />
+          name:
+          <input type="text" value={newName} onChange={handleName} />
+        </div>
+        <div>
+          phone:
+          <input type="tel" value={newPhone} onChange={handlePhone} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <div>debug: {newName}</div> 
+      <div>debug: {newName}</div>
       <h2>Numbers</h2>
-        <Numbers persons={persons} />
+      <Numbers persons={persons} />
     </div>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- exercises/part02/2.6-2.10_phonebook/src/Numbers.jsx
+++ exercises/part02/2.6-2.10_phonebook/src/Numbers.jsx
@@ -2,7 +2,7 @@ const Numbers = ({ persons }) => {
   return (
     <ul>
       {persons.map((person, index) => (
-        <li key={index}>{person.name}</li>
+        <li key={index}>{person.name}{person.phone ? " - " + person.phone : ""}</li>
       ))}
     </ul>
   )
